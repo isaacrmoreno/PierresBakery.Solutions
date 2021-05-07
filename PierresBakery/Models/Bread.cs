49abfa4a074e86b97400c9b24a9e360d9037c727
Isaac Moreno <ipdxcreative@gmail.com>
2021-05-07 11:44:45 -0700

    renamed method name for clarity

---
+++ PierresBakery/Models/Bread.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace PierresBakery.Models
+{
+  public class Bread
+  {
+    public int BreadQty { get; set; }
+
+    public Bread(int breadQty)
+    {
+      BreadQty = breadQty;
+    }
+    public int TotalBreadPrice()
+    {
+      if (BreadQty % 3 == 0)
+      {
+        totalPrice = ()
+      }
+    }
+
+  }
+}
