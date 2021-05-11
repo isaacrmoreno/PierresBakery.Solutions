c84aed134f2efd05a6409a310c8a30a8b98e4924
Isaac Moreno <ipdxcreative@gmail.com>
2021-05-10 21:46:26 -0700

    add error handling

--- PierresBakery/Program.cs
+++ PierresBakery/Program.cs
@@ -13,10 +13,29 @@ namespace PierresBakery.Models
       Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
       Console.Write("How many loafs of bread would you you like?:");
       string userBreadInput = Console.ReadLine();
-      int intBreadInput = int.Parse(userBreadInput);
+      // int intBreadInput = int.Parse(userBreadInput);
+      int intBreadInput = 0;
+      try
+      {
+        intBreadInput = int.Parse(userBreadInput);
+      }
+      catch (FormatException)
+      {
+        Console.WriteLine("Error: Please enter a number.");
+        Main();
+      }
       Console.Write("How many pastry's would you like?:");
       string userPastryInput = Console.ReadLine();
-      int intPastryInput = int.Parse(userPastryInput);
+      int intPastryInput = 0;
+      try
+      {
+        intPastryInput = int.Parse(userPastryInput);
+      }
+      catch (FormatException)
+      {
+        Console.WriteLine("Error: Please enter a number.");
+        Main();
+      }
       int TotalOrderPrice = Bread.CalculateBreadPrice(intBreadInput) + Pastry.CalculatePastryPrice(intPastryInput);
       Console.WriteLine($"Great! Your total price will be: {TotalOrderPrice}!");
       Main();
