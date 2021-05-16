fcef9036488515ef063bd387e2331215b97cd60a
Isaac Moreno <ipdxcreative@gmail.com>
2021-05-16 15:17:46 -0700

    uncomment other test

--- PierresBakery.Tests/ModelTests/BreadTest.cs
+++ PierresBakery.Tests/ModelTests/BreadTest.cs
@@ -12,36 +12,36 @@ namespace PierresBakery.Tests
       Bread newBread = new(1);
       Assert.AreEqual(typeof(Bread), newBread.GetType());
     }
-    // [TestMethod]
-    // public void CalculateBreadPrice_GetsPriceOfOneBread_BreadTotal()
-    // {
-    //   int BreadPrice = Bread.CalculateBreadPrice(1);
-    //   Assert.AreEqual(BreadPrice, 5);
-    // }
-    // [TestMethod]
-    // public void CalculateBreadPrice_GetsPriceOfTwoBread_BreadTotal()
-    // {
-    //   int BreadPrice = Bread.CalculateBreadPrice(2);
-    //   Assert.AreEqual(BreadPrice, 10);
-    // }
-    // [TestMethod]
-    // public void CalculateBreadPrice_GetsPriceOfThreeBread_BreadTotal()
-    // {
-    //   int BreadPrice = Bread.CalculateBreadPrice(3);
-    //   Assert.AreEqual(BreadPrice, 10);
-    // }
-    // [TestMethod]
-    // public void CalculateBreadPrice_GetsPriceOfNineBread_BreadTotal()
-    // {
-    //   int BreadPrice = Bread.CalculateBreadPrice(9);
-    //   Assert.AreEqual(BreadPrice, 30);
-    // }
-    // [TestMethod]
-    // public void CalculateBreadPrice_GetsPriceOf268Bread_BreadTotal()
-    // {
-    //   int BreadPrice = Bread.CalculateBreadPrice(268);
-    //   Assert.AreEqual(BreadPrice, 1340);
-    // }
+    [TestMethod]
+    public void CalculateBreadPrice_GetsPriceOfOneBread_BreadTotal()
+    {
+      int BreadPrice = Bread.CalculateBreadPrice(1);
+      Assert.AreEqual(BreadPrice, 5);
+    }
+    [TestMethod]
+    public void CalculateBreadPrice_GetsPriceOfTwoBread_BreadTotal()
+    {
+      int BreadPrice = Bread.CalculateBreadPrice(2);
+      Assert.AreEqual(BreadPrice, 10);
+    }
+    [TestMethod]
+    public void CalculateBreadPrice_GetsPriceOfThreeBread_BreadTotal()
+    {
+      int BreadPrice = Bread.CalculateBreadPrice(3);
+      Assert.AreEqual(BreadPrice, 10);
+    }
+    [TestMethod]
+    public void CalculateBreadPrice_GetsPriceOfNineBread_BreadTotal()
+    {
+      int BreadPrice = Bread.CalculateBreadPrice(9);
+      Assert.AreEqual(BreadPrice, 30);
+    }
+    [TestMethod]
+    public void CalculateBreadPrice_GetsPriceOf268Bread_BreadTotal()
+    {
+      int BreadPrice = Bread.CalculateBreadPrice(268);
+      Assert.AreEqual(BreadPrice, 1340);
+    }
 
   }
 }
--- PierresBakery.Tests/ModelTests/PastryTest.cs
+++ PierresBakery.Tests/ModelTests/PastryTest.cs
@@ -12,35 +12,35 @@ namespace PierresBakery.Tests
       Pastry newPastry = new(1);
       Assert.AreEqual(typeof(Pastry), newPastry.GetType());
     }
-    // [TestMethod]
-    // public void CalculatePastryPrice_GetsPriceOfOnePastry_PastryTotal()
-    // {
-    //   int PastryPrice = Pastry.CalculatePastryPrice(1);
-    //   Assert.AreEqual(PastryPrice, 2);
-    // }
-    // [TestMethod]
-    // public void CalculatePastryPrice_GetsPriceOfTwoPastry_PastryTotal()
-    // {
-    //   int PastryPrice = Pastry.CalculatePastryPrice(2);
-    //   Assert.AreEqual(PastryPrice, 4);
-    // }
-    // [TestMethod]
-    // public void CalculatePastryPrice_GetsPriceOfThreePastry_PastryTotal()
-    // {
-    //   int PastryPrice = Pastry.CalculatePastryPrice(3);
-    //   Assert.AreEqual(PastryPrice, 5);
-    // }
-    // [TestMethod]
-    // public void CalculatePastryPrice_GetsPriceOfNinePastry_PastryTotal()
-    // {
-    //   int PastryPrice = Pastry.CalculatePastryPrice(9);
-    //   Assert.AreEqual(PastryPrice, 15);
-    // }
-    // [TestMethod]
-    // public void CalculatePastryPrice_GetsPriceOf362Pastry_PastryTotal()
-    // {
-    //   int PastryPrice = Pastry.CalculatePastryPrice(362);
-    //   Assert.AreEqual(PastryPrice, 724);
-    // }
+    [TestMethod]
+    public void CalculatePastryPrice_GetsPriceOfOnePastry_PastryTotal()
+    {
+      int PastryPrice = Pastry.CalculatePastryPrice(int 1);
+      Assert.AreEqual(PastryPrice, 2);
+    }
+    [TestMethod]
+    public void CalculatePastryPrice_GetsPriceOfTwoPastry_PastryTotal()
+    {
+      int PastryPrice = Pastry.CalculatePastryPrice(2);
+      Assert.AreEqual(PastryPrice, 4);
+    }
+    [TestMethod]
+    public void CalculatePastryPrice_GetsPriceOfThreePastry_PastryTotal()
+    {
+      int PastryPrice = Pastry.CalculatePastryPrice(3);
+      Assert.AreEqual(PastryPrice, 5);
+    }
+    [TestMethod]
+    public void CalculatePastryPrice_GetsPriceOfNinePastry_PastryTotal()
+    {
+      int PastryPrice = Pastry.CalculatePastryPrice(9);
+      Assert.AreEqual(PastryPrice, 15);
+    }
+    [TestMethod]
+    public void CalculatePastryPrice_GetsPriceOf362Pastry_PastryTotal()
+    {
+      int PastryPrice = Pastry.CalculatePastryPrice(362);
+      Assert.AreEqual(PastryPrice, 724);
+    }
   }
 }
